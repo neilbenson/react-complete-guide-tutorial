78ff118814e3c23e0d66415229790734e0a651bb
hellbrandt <hellbrandt@googlemail.com>
2021-05-07 12:37:41 +0100

    Add ExpenseDate css and other tweaks

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,10 @@
-import CalendarDay from "./CalendarDay";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
-      <CalendarDay {...props} />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
