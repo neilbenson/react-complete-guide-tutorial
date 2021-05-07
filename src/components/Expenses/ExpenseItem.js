e37b53f6d0434e5c011b445d84867d2f59cef479
hellbrandt <hellbrandt@googlemail.com>
2021-05-07 16:32:03 +0100

    Reorganise project files

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../ui/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
