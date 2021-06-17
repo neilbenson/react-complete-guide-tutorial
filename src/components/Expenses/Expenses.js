1fc99d4f5c86408c8202e1be79c5ec9061cbe84a
hellbrandt <hellbrandt@googlemail.com>
2021-06-17 15:14:59 +0100

    Pass data for ExpensesFilter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [year, updateYear] = useState("2020");
+
+  const yearToFilterHander = (enteredYear) => {
+    updateYear(enteredYear);
+    console.log(year);
+  };
+
   const expensesMap = props.expenses.map((expense) => {
     return (
       <ExpenseItem
@@ -14,7 +22,12 @@ const Expenses = (props) => {
     );
   });
 
-  return <Card className="expenses">{expensesMap}</Card>;
+  return (
+    <div>
+      <ExpensesFilter onFilterByYear={yearToFilterHander} selectedYear={year} />
+      <Card className="expenses">{expensesMap}</Card>
+    </div>
+  );
 };
 
 export default Expenses;
