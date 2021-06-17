6b1f9ffc40ec17ecfaa28ae029a4530e48713915
hellbrandt <hellbrandt@googlemail.com>
2021-06-17 12:41:31 +0100

    Demo pass data between child & parent with props

--- src/App.js
+++ src/App.js
@@ -26,6 +26,11 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  }
+
   // Alternative to JSX code
   // return React.createElement(
   //   "div",
@@ -36,7 +41,7 @@ const App = () => {
 
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses expenses={expenses} />
     </div>
   );
