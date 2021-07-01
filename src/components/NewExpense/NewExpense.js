b1a7966289612175d1f09cab8c6c8b75bf2c3c88
hellbrandt <hellbrandt@googlemail.com>
2021-07-01 16:15:02 +0100

    My solution to show/hide new expenses form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,45 @@
-import React from 'react'; // Optional, not required in latest React
-import './NewExpense.css';
+import React, { useState } from "react"; // Optional, not required in latest React
+import "./NewExpense.css";
 
-import ExpenseForm from './ExpenseForm';
+import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
-    const saveExpenseDataHandler = (enteredExpenseData) => {
-        const expenseData = {
-            ...enteredExpenseData,
-            id: Math.random().toString()
-        }
-        console.log(expenseData);
-        props.onAddExpense(expenseData);
-    };
+  const [showNewExpense, setShowNewExpense] = useState(true);
 
-    return <div className="new-expense">
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    console.log(expenseData);
+    props.onAddExpense(expenseData);
+    switchNewExpenseHandler();
+  };
+
+  const switchNewExpenseHandler = () => {
+    setShowNewExpense(!showNewExpense);
+  };
+
+  if (showNewExpense) {
+    return (
+      <div className="new-expense">
+        <div className="new-expense__actions centred">
+          <button type="button" onClick={switchNewExpenseHandler}>
+            Add New Expense
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm
+        onCancelNewExpense={switchNewExpenseHandler}
+        onSaveExpenseData={saveExpenseDataHandler}
+      />
     </div>
+  );
 };
 
 export default NewExpense;
